a08f0cccb0f92a92b7264cb3543a27599e210c24
andreykarganov <karganov.an@yandex.ru>
2026-07-27 15:52:48 +0300

    new

---
+++ internal/booking/domain.go
@@ -0,0 +1,20 @@
+package booking
+
+import "errors"
+
+var (
+	ErrSeatAlreadyBooked = errors.New("seat is already booked")
+)
+
+type Booking struct {
+	ID string
+	MovieID string
+	SeatID string
+	UserID string
+	Status string
+}
+
+type BookingStore interface {
+	Book(b Booking) error
+	ListBookings(movieID string) []Booking
+}
